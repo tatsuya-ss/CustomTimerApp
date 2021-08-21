3ccdeee8d1957ee848fc9ed81246951772909384
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-08-21 18:54:58 +0900

    CustomTimer画面のレイアウト作成

--- CustomTimerApp/Controllers/CustomTimerViewController.swift
+++ CustomTimerApp/Controllers/CustomTimerViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class CustomTimerViewController: UIViewController {
-
+    @IBOutlet private weak var timerNameTextField: UITextField!
+    @IBOutlet private weak var TimerContentsCollectionView: UICollectionView!
+    @IBOutlet private weak var timePickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction private func saveTimerButtonTapped(_ sender: Any) {
     }
-    */
-
+    @IBAction private func cancelButtonTapped(_ sender: Any) {
+    }
 }
