eae85ab79c93081f589ea6929b23f332780ffbd4
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-08-21 18:12:40 +0900

    Timer画面のレイアウト作成とアウトレット接続

--- CustomTimerApp/Controllers/TimerViewController.swift
+++ CustomTimerApp/Controllers/TimerViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    @IBOutlet weak var timerCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addTimerButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func settingButtonTapped(_ sender: Any) {
+    }
+    
 }
