ee5929a911876498cd0a0fc694a59f665f1f6458
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-08 11:39:48 +0900

    タイマー一覧表示用のXibを作成

---
+++ CustomTimerApp/Views/Xibs/TimerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TimerCollectionViewCell.swift
+//  CustomTimerApp
+//
+//  Created by 坂本龍哉 on 2021/09/08.
+//
+
+import UIKit
+
+class TimerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
