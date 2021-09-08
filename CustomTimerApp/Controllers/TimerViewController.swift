76b74cbc555502e5c31c7d45b97f25be2c75e87b
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-08 12:14:36 +0900

    デモデータを表示させる処理を作成

--- CustomTimerApp/Controllers/TimerViewController.swift
+++ CustomTimerApp/Controllers/TimerViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class TimerViewController: UIViewController {
-    @IBOutlet weak var timerCollectionView: UICollectionView!
+final class TimerViewController: UIViewController {
+    
+    private enum Section {
+        case mainTimer
+    }
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Section, CustomTimerComponent>! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureHierarchy()
+        configureDataSource()
+        displayAllTimer()
+        
     }
     
     @IBAction func addTimerButtonTapped(_ sender: Any) {
@@ -26,4 +36,63 @@ class TimerViewController: UIViewController {
         present(settingNavVC, animated: true, completion: nil)
     }
     
+    private func displayAllTimer(animated: Bool = true) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, CustomTimerComponent>()
+        snapshot.appendSections([.mainTimer])
+        let demo = [CustomTimerComponent(name: "ストレッチ", timeInfomations: [TimeInfomation(time: 10, photo: nil, text: nil)]),
+                    CustomTimerComponent(name: "スクワット", timeInfomations: [TimeInfomation(time: 10, photo: nil, text: nil)]),
+                    CustomTimerComponent(name: "ランニング", timeInfomations: [TimeInfomation(time: 10, photo: nil, text: nil)])]
+        snapshot.appendItems(demo)
+        dataSource.apply(snapshot, animatingDifferences: animated)
+    }
+
+}
+
+extension TimerViewController: UICollectionViewDelegate {
+    
+}
+
+extension TimerViewController {
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(120))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitem: item,
+                                                       count: 1)
+        let groupSpace = CGFloat(20)
+        group.contentInsets = NSDirectionalEdgeInsets(top: groupSpace, leading: groupSpace, bottom: groupSpace, trailing: groupSpace)
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    private func configureHierarchy() {
+        collectionView.collectionViewLayout = createLayout()
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.register(TimerCollectionViewCell.nib,
+                                forCellWithReuseIdentifier: TimerCollectionViewCell.identifier)
+    }
+
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, CustomTimerComponent>(collectionView: collectionView, cellProvider: {
+            collectionView, indexPath, customTimerComponent in
+            guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: TimerCollectionViewCell.identifier, for: indexPath
+            ) as? TimerCollectionViewCell else { fatalError("セルが見つかりませんでした") }
+            cell.configure(timerName: customTimerComponent.name)
+            cell.backgroundColor = .gray
+            cell.layer.cornerRadius = 10
+            return cell
+        })
+    }
+
 }
