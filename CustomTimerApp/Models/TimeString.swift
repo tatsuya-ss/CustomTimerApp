04da7ce8af760d2f02efc342ac07ab20873cd976
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-09-26 15:59:51 +0900

    ２桁表示に変換する構造体を作成

--- CustomTimerApp/Models/TimeString.swift
+++ CustomTimerApp/Models/TimeString.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+
+struct TimeString {
+    
+    func makeTimeString(hour: Int, minute: Int, second: Int) -> String {
+        let stringHour = makeTwoDigits(time: hour)
+        let stringMinute = makeTwoDigits(time: minute)
+        let stringSecond = makeTwoDigits(time: second)
+        return "\(stringHour):\(stringMinute):\(stringSecond)"
+    }
+    
+    private func makeTwoDigits(time: Int) -> String {
+        if time < 10 {
+            return "0\(time)"
+        } else {
+            return String(time)
+        }
+    }
+    
+}
