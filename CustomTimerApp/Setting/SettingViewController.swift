46f3c49742acda3a9e9f069d845e110be55b3773
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-10 17:49:08 +0900

    ListCellを表示する処理作成

--- CustomTimerApp/Setting/SettingViewController.swift
+++ CustomTimerApp/Setting/SettingViewController.swift
@@ -10,21 +10,83 @@ import UIKit
 final class SettingViewController: UIViewController {
     
     private enum Section: CaseIterable {
-        case setting
         case app
     }
     
+    private enum ApplicationCellType: CaseIterable {
+        case operation
+        case evaluation
+        case inquiry
+        case share
+        
+        var title: String {
+            switch self {
+            case .operation:
+                return "操作方法"
+            case .evaluation:
+                return "このアプリを評価する"
+            case .inquiry:
+                return "お問い合わせ"
+            case .share:
+                return "このアプリをシェアする"
+            }
+        }
+    }
+
     @IBOutlet private weak var collectionView: UICollectionView!
     
     private var dataSource: UICollectionViewDiffableDataSource<Section, String>! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureHierarchy()
+        configureDataSource()
     }
     
     @IBAction private func stopButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+}
+
+extension SettingViewController: UICollectionViewDelegate {
+    
+}
+
+extension SettingViewController {
+    
+    private func createListLayout() -> UICollectionViewLayout {
+        let config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+    
+    private func configureHierarchy() {
+        collectionView.collectionViewLayout = createListLayout()
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+    }
+    
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, String> { cell, indexPath, item in
+            var content = cell.defaultContentConfiguration() // デフォルトのListCellを取得
+            content.text = item
+            cell.contentConfiguration = content
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, String>(collectionView: collectionView, cellProvider: {
+            collectionView, indexPath, itemIdentifier -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration,
+                                                                for: indexPath,
+                                                                item: itemIdentifier)
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.app])
+        ApplicationCellType.allCases.forEach {
+            snapshot.appendItems([$0.title], toSection: .app)
+        }
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+
 }
