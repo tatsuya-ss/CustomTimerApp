98dc6a0148009df53c20a34f83a6ec9d3ccc3ed8
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-15 16:48:02 +0900

    EditTimerCollectionViewのlayout作成

--- CustomTimerApp/EditTimer/Cells/EditTimerCollectionViewFlowLayout.swift
+++ CustomTimerApp/EditTimer/Cells/EditTimerCollectionViewFlowLayout.swift
@@ -5,4 +5,21 @@
 //  Created by 坂本龍哉 on 2021/10/15.
 //
 
-import Foundation
+import UIKit
+
+final class EditTimerCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+    override func prepare() {
+        guard let cv = collectionView else { return }
+        scrollDirection = .horizontal
+
+        let availableWidth = cv.bounds.inset(by: cv.layoutMargins).size.width
+        let cellWidth = (availableWidth / 3).rounded(.down)
+                
+        let cellHeight = cv.layer.frame.height * 0.9
+        
+        self.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+    
+}
