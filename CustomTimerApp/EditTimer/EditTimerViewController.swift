44d774161cdfb876880d8373c05601c027ba1c4a
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-15 17:11:04 +0900

    保存済みのタイマーをセルに表示する処理を作成

--- CustomTimerApp/EditTimer/EditTimerViewController.swift
+++ CustomTimerApp/EditTimer/EditTimerViewController.swift
@@ -19,9 +19,12 @@ final class EditTimerViewController: UIViewController {
         self.customTimerComponent = customTimerComponent
     }
     
+    private var selectedIndexPath: IndexPath = [0, 0]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupCollectionView()
+        collectionView.reloadData()
     }
 
     @IBAction private func saveButtonDidTapped(_ sender: Any) {
@@ -38,6 +41,52 @@ final class EditTimerViewController: UIViewController {
     
     @IBAction private func selectPhotoButtonDidTapped(_ sender: Any) {
         
+    }
+    
+    private func makePhotoImage(timeInfomation: TimeInfomation) -> UIImage? {
+        guard let imageData = timeInfomation.photo,
+              let image = UIImage(data: imageData) else { return UIImage(systemName: "timer") }
+        return image
+    }
+
+}
+
+extension EditTimerViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        customTimerComponent.timeInfomations.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: EditTimerCollectionViewCell.identifier, for: indexPath
+        ) as? EditTimerCollectionViewCell else { fatalError("セルが見つかりません") }
+        let timeString = customTimerComponent.timeInfomations[indexPath.item].time.makeTimeString()
+        let image = makePhotoImage(timeInfomation: customTimerComponent.timeInfomations[indexPath.item])
+        cell.configure(image: image, timeString: timeString)
+        indexPath == selectedIndexPath
+            ? cell.selectedCell()
+            : cell.unselectedCell()
+        return cell
+    }
+    
+}
+
+extension EditTimerViewController: UICollectionViewDelegate {
+    
+}
+
+extension EditTimerViewController {
+    
+    private func setupCollectionView() {
+        collectionView.collectionViewLayout = EditTimerCollectionViewFlowLayout()
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(EditTimerCollectionViewCell.nib,
+                                forCellWithReuseIdentifier: EditTimerCollectionViewCell.identifier)
     }
     
 }
