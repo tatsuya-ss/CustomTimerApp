9c57d87697c8563a5000ab3552ccf6ca10bb5bc1
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-15 16:15:55 +0900

    EditTimer画面を作成

---
+++ CustomTimerApp/EditTimer/EditTimerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EditTimerViewController.swift
+//  CustomTimerApp
+//
+//  Created by 坂本龍哉 on 2021/10/15.
+//
+
+import UIKit
+
+final class EditTimerViewController: UIViewController {
+
+    @IBOutlet private weak var timerNameTextField: UITextField!
+    @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var timePickerView: UIPickerView!
+    @IBOutlet private weak var plusButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction private func saveButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func cancelButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func plusButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction private func selectPhotoButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+}
