9915a8f3cc9ee23f655e0b2c0371007092cb8bc4
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-15 16:47:30 +0900

    CellのxibとFlowLayoutファイル作成

---
+++ CustomTimerApp/EditTimer/Cells/EditTimerCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EditTimerCollectionViewCell.swift
+//  CustomTimerApp
+//
+//  Created by 坂本龍哉 on 2021/10/15.
+//
+
+import UIKit
+
+class EditTimerCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
