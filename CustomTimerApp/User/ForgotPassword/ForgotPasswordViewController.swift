97dfd2a8d99bec1cf03304616cd439aa3db53954
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-10-30 12:19:37 +0900

    ForgotPassword画面作成

---
+++ CustomTimerApp/User/ForgotPassword/ForgotPasswordViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ForgotPasswordViewController.swift
+//  CustomTimerApp
+//
+//  Created by 坂本龍哉 on 2021/10/30.
+//
+
+import UIKit
+
+final class ForgotPasswordViewController: UIViewController {
+
+    @IBOutlet private weak var mailAddressTextField: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction private func sendButtonDidTapped(_ sender: Any) {
+        
+    }
+    
+}
