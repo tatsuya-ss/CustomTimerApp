b14118d8d7040a0c60f83da7d21e6643eeb321bb
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-11-13 17:37:57 +0900

    タイマー情報フェッチ処理

--- CustomTimerApp/Domain/Timer/TimerDataStore.swift
+++ CustomTimerApp/Domain/Timer/TimerDataStore.swift
@@ -58,11 +58,15 @@ final class TimerDataStore: TimerDataStoreProtocol {
     
     func savePhoto(customTimer: CustomTimerComponent,
                    completion: @escaping StoreResultHandler<Any?>) {
-        guard let user = user else { return }
+        guard let user = user else {
+            completion(.failure(DataBaseError.unknown))
+            return
+        }
         let storageRef = Storage.storage().reference()
         
         let dispatchGroup = DispatchGroup()
-        let dispatchQueue = DispatchQueue(label: "dataStoreSavePhoto", attributes: .concurrent)
+        let dispatchQueue = DispatchQueue(label: .savePhotoQueueLabel,
+                                          attributes: .concurrent)
         
         customTimer.timeInfomations.forEach {
             if let photoData =  $0.photo {
@@ -84,4 +88,78 @@ final class TimerDataStore: TimerDataStoreProtocol {
         
     }
     
+    func fetch(completion: @escaping StoreResultHandler<[DataBaseCustomTimer]>) {
+        guard let user = user else {
+            completion(.failure(DataBaseError.unknown))
+            return
+        }
+        
+        let timerCollectionRef = db.collection("user").document(user.uid).collection("timer")
+        timerCollectionRef.getDocuments() { querySnapshot, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let documents = querySnapshot?.documents else {
+                completion(.failure(DataBaseError.unknown))
+                return
+            }
+            
+            let dispatchGroup = DispatchGroup()
+            let dispatchQueue = DispatchQueue(label: .fetchTimerDataQueueLabel,
+                                              attributes: .concurrent)
+            
+            var timers: [DataBaseCustomTimer] = []
+            var dataBaseError: Error?
+            dispatchQueue.async(group: dispatchGroup) {
+                documents.forEach { document in
+                    let result = Result { try document.data(as: DataBaseCustomTimer.self) }
+                    switch result {
+                    case .success(let timer):
+                        if let timer = timer {
+                            timers.append(timer)
+                        } else { fatalError() }
+                    case .failure(let error):
+                        dataBaseError = error
+                    }
+                }
+            }
+            
+            dispatchGroup.notify(queue: .main) {
+                if let dataBaseError = dataBaseError {
+                    completion(.failure(dataBaseError))
+                } else {
+                    completion(.success(timers))
+                }
+            }
+            
+        }
+    }
+    
+    func fetchPhotos(timerId: String, photoId: String, completion: @escaping StoreResultHandler<URL>) {
+        guard let user = user else {
+            completion(.failure(DataBaseError.unknown))
+            return
+        }
+        let photoRef =  Storage.storage().reference().child("users/\(user.uid)/timers/\(timerId)/\(photoId).jpg")
+        let cachesDirectoryPath = NSSearchPathForDirectoriesInDomains(.cachesDirectory,
+                                                                      .userDomainMask,
+                                                                      true)[0]
+        let photoName = "\(photoId).jpg"
+        let cachesURL = URL(fileURLWithPath: "\(cachesDirectoryPath)/\(photoName)")
+        let downloadTask = photoRef.write(toFile: cachesURL) { url, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(cachesURL))
+            }
+        }
+        downloadTask.resume()
+    }
+    
+}
+
+private extension String {
+    static let savePhotoQueueLabel = "CustomTimerApp.SavaPhotoQueue"
+    static let fetchTimerDataQueueLabel = "CustomTimerApp.FetchTimerDataQueue"
 }
