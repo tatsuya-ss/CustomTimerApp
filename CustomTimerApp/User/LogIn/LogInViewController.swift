00841dc9892c1eef14992344cd70bbd63fcd57dc
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-11-24 10:29:11 +0900

    キーボード表示時のレイアウト変更

--- CustomTimerApp/User/LogIn/LogInViewController.swift
+++ CustomTimerApp/User/LogIn/LogInViewController.swift
@@ -9,12 +9,14 @@ import UIKit
 
 extension LogInViewController: ShowAlertProtocol { }
 extension LogInViewController: UIButtonLayoutProtocol { }
+extension LogInViewController: ChangeLayoutProtocol { }
 
 final class LogInViewController: UIViewController {
 
     @IBOutlet private weak var mailAddressTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
     @IBOutlet private weak var logInButton: UIButton!
+    @IBOutlet private weak var logInStackView: UIStackView!
     
     private var userUseCase: UserUseCaseProtocol!
     private let indicator = Indicator()
@@ -22,9 +24,15 @@ final class LogInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupButton(button: logInButton, layout: LogInButtonLayout())
+        setupNotification()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction private func logInButtonDidTapped(_ sender: Any) {
+        self.view.endEditing(true)
         guard let mail = mailAddressTextField.text,
               let password = passwordTextField.text else { return }
         indicator.show(flashType: .progress)
@@ -34,10 +42,14 @@ final class LogInViewController: UIViewController {
             case .failure(let error):
                 self?.indicator.flash(flashType: .error) {
                     self?.showErrorAlert(title: error.errorMessage)
+                    self?.view.endEditing(true)
                 }
             case .success:
                 self?.indicator.flash(flashType: .success) {
-                    self?.dismiss(animated: true, completion: nil)
+                    let timerVC = TimerViewController.instantiate()
+                    let navigationVC = UINavigationController(rootViewController: timerVC)
+                    navigationVC.modalPresentationStyle = .fullScreen
+                    self?.present(navigationVC, animated: true, completion: nil)
                 }
             }
         }
@@ -50,6 +62,30 @@ final class LogInViewController: UIViewController {
     
 }
 
+// MARK: - func
+extension LogInViewController {
+    
+    private func setupNotification() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keybordWillShow),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    @objc private func keybordWillShow(notification: Notification) {
+        let signUpButtonBottomMaxY = logInStackView.frame.maxY
+        changeLayoutWhenKeyboardShow(notification: notification, positionMaxY: signUpButtonBottomMaxY)
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        undoOriginalViewFrame(notification: notification)
+    }
+
+}
 // MARK: - instantiate
 extension LogInViewController {
     
